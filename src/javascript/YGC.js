0498391b353bdf7ccde1603ee88f878f2ce64e40
Rahul Balivada <rahulvarun.b@gmail.com>
2023-07-23 16:08:17 -0500

    test 001

--- src/javascript/YGC.js
+++ src/javascript/YGC.js
@@ -10,43 +10,16 @@ const YGC = () => {
     <div className="container">
 
     <div>
-    <p className="center-align c-gray"><img src="/images/ygc/2023/YGC_2023.png"alt="YGC 2023" width="300" height="150" className="rounded center-align" ></img></p>
-    </div>
-
-    <p >&nbsp;&nbsp;</p>
-    <div>
-    <h3 className="center-align c-gray"><b >We are bringing together high school students that believe in working together to positively impact our community and world. </b></h3>
-    </div>
-
-    <div className="row">
-
-    <div className="col-sm-4 ">
-
-    </div>
-
-    <div className="col-sm-4 bg-info border">
-
-    <p ></p>
-    <h3 className="center-align c-white"   ><a href="https://tinyurl.com/ygc-overview"><b><span className="c-gray">Program Overview</span></b></a></h3>
-    <p ></p>
-    </div>
-
-    <div className="col-sm-4 ">
-
-    </div>
-
+    <p className="center-align"><img src="/images/ygc/2023/YGC_2023.png"alt="YGC 2023" width="200" height="100" className="rounded center center-align" ></img></p>
     </div>
 
     <div>
-    <h3 className="center-align c-gray"><b >Students will gain valuable personal leadership and project management knowledge and skills while building a network of peers and leading a project for a non-profit group in our community.</b></h3>
+    <h3 className="center-align"><b >Class of 2023</b></h3>
     </div>
 
-    <p >&nbsp;&nbsp;</p>
-    <p >&nbsp;&nbsp;</p>
-    <p >&nbsp;&nbsp;</p>
-    <p >&nbsp;&nbsp;</p>
-    <p >&nbsp;</p>
-    <p >&nbsp;&nbsp;</p>
+    <div><hr class="hr-line"></hr></div>
+
+    
     </div>
 
 
--- src/javascript/YICU/yicu2020.js
+++ src/javascript/YICU/yicu2020.js
@@ -7,13 +7,112 @@ const yicu2020 = () => {
 
   return(
 
+
+
     <div className="container">
     <YICUnav ></YICUnav>
     <div>
-    <Wip ></Wip>
+    <h3 className="logo"><img src="/images/base/Logo.jpg" alt="" width="40" height="40" className="rounded"/>  for a <span className="cRed">better</span> tomorrow </h3>
+    <h3 className="center-align">YICU Service Awards 2020</h3>
+    <h3 className="center-align">Winners</h3>
+    <p >&nbsp;</p>
+    </div>
+
+    <div><hr class="hr-line"></hr></div>
+
+    <p >&nbsp;</p>
+    <div className="row">
+
+    <div className="col-sm">
+
+    <h4 className="center-align"><b >Individual Awards</b></h4>
     </div>
 
     </div>
+
+    <div className="row">
+
+    <div className="col-sm">
+
+    <h6 className="center-align cGray">Shashi Salavath</h6>
+    <h6 className="center-align cGray">Isha Gollapudi</h6>
+    <h6 className="center-align cGray">Satvika Boyina</h6>
+    </div>
+
+    <div className="col-sm">
+
+    <h6 className="center-align cGray">Erica Rosenberger</h6>
+    <h6 className="center-align cGray">Rachel Dawson</h6>
+    <h6 className="center-align cGray">Jasmyn Jordan</h6>
+    </div>
+
+    <div className="col-sm">
+
+    <h6 className="center-align cGray">Yvin Shin</h6>
+    <h6 className="center-align cGray">Gavin Cunningham</h6>
+    <h className="center-align cGray">Heather Dawson</h>
+    </div>
+
+    </div>
+
+    <div><hr className="line2"></hr></div>
+
+    <div className="row">
+
+    <div className="col-sm">
+
+    <h4 className="center-align"><b >Team Award</b></h4>
+    <h6 className="center-align">The Normal West Black Student Union</h6>
+    </div>
+
+    </div>
+
+    <div><hr className="line2"></hr></div>
+
+    <div className="row">
+
+    <div className="col-sm">
+
+    <h4 className="center-align"><b >Advocate Award</b></h4>
+    <h6 className="center-align">Helping Hands Community Center (HHCC)</h6>
+    </div>
+
+    </div>
+
+    <div><hr className="line2"></hr></div>
+
+    <div className="row">
+
+    <div className="col-sm">
+
+    <h4 className="center-align"><b >Mentor Award</b></h4>
+    <h6 className="center-align">Shri Narayan Chandak</h6>
+    </div>
+
+    </div>
+
+    <div><hr className="line2"></hr></div>
+
+    <div className="row">
+    <h4 className="center-align"><b >FBT is excited to make a $250 donation towards the non-profits chosen by the award winners.</b></h4>
+    </div>
+
+    <div className="row">
+
+    <div className="col-sm">
+
+    <h4 className="center-align"><a href="https://www.facebook.com/4ABetterTomorrow/videos/376058036781953/"><b>YICU 2020 Event Video</b></a></h4>
+    <p className="center-align"><a href="https://www.facebook.com/4ABetterTomorrow/videos/376058036781953/"><img src="Images/2020/YICU2020-Play.png" alt="" width="50%" className="rounded" /></a></p>
+    <p >&nbsp;&nbsp;</p>
+    </div>
+
+    </div>
+
+    <div><hr class="hr-line"></hr></div>
+
+  
+    </div>
+
 
 
   );
