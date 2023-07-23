045ddd4133edfad919007ce4769defec7d1aab52
Rahul Balivada <rahulvarun.b@gmail.com>
2023-07-23 16:12:43 -0500

    ygc class of 2023 re-done

--- src/javascript/YGC.js
+++ src/javascript/YGC.js
@@ -19,7 +19,147 @@ const YGC = () => {
 
     <div><hr class="hr-line"></hr></div>
 
-    
+    <p >&nbsp;&nbsp;</p>
+    <div className="row">
+
+    <div className="col-sm">
+
+    <table className="table table-borderless" >
+
+    <tbody>
+
+    <tr className="row">
+    <td className="col-md-12 imgdiv center-align"><img src="/images/ygc/2023/YGC_Class_2023.jpg"alt="Class of 2023"  className="rounded" width="70%"></img></td>
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Saaketh.jpg"  alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Saaketh</b></span> is a hardworking individual that loves to get things done. He is eager to learn and grow and loves to succeed. He motivates those around him and encourages them to be the best version of themselves that they can be. He knows that he cannot change the world overnight, but rather that everything should be done one step at a time.</h6>
+    <h6 ><i >Quote that best describes Saaketh: "Difficulties strengthen the mind as labor does the body" - Seneca</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Vedha_Giriprakash.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Vedha Giriprakash</b></span> is a 15 year old NCHS student. In her free time, she enjoys exploring new recipes, playing her viola, among other fun hobbies!</h6>
+    <h6 ><i >Quote that best describes Vedha: "How people treat you is a reflection of themselves."</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Prajna.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Prajna</b></span> is a Senior at Normal Community High School who aspires to make a difference in her community and more. She is currently a Co-Vice President of Not In our School (NIOS) and the Social Media Manager for NIOS and Spanish Club. Outside of School, Prajna is an active member of the NIOS Steering Committee and is a tutor at Gen3 Learning.</h6>
+    <h6 ><i >Quote that best describes Prajna: "Success is not a destination, it's a journey. There are no full stops, only commas".</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Atharva_Shenoy.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Atharva Shenoy</b></span> is a rising senior at Normal Community High School. He enjoys Math and Band. He began playing trumpet since his elementary school days and has participated in numerous band concerts. In the community, Atharva has volunteered at Midwest Food Bank and Children’s Discovery Museum. He also volunteers for annual food drives and fundraising activities for the local chapter of Sewa International Inc. nonprofit. Atharva is looking forward to joining YGC and learning more about the community while honing his leadership skills.</h6>
+    <h6 ><i >Quote that best describes Atharva: “Education is our passport to the future, for tomorrow belongs to the people who prepare for it today.” - Malcolm X </i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Harshal.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Harshal</b></span> has always been a curious person and enjoys hands-on learning. He is also very competitive and participates in numerous events ranging from public speaking to mathematics competitions. Additionally, Harshal has always had a strong interest in science and loves to engage in scientific research opportunities. Currently a rising sophomore, Harshal has ambitious goals for the future and hopes to have a great impact on his community.</h6>
+    <h6 ><i >Quote that best describes Harshal: “Start where you are, with what you have. Make something of it and never be satisfied.” - George Washington Carver</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Renny.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Renny</b></span> is about to be a highschool senior. He enjoys piano, hanging out with friends, and playing video games. In the coming year he hopes to make a difference in the current clubs and activities he’s participating in. Renny is aiming to be a chemical engineer at the university of Urbana-Champaign.</h6>
+    <h6 ><i >Quote that best describes Renny: “In the midst of chaos there is always opportunity” - Sun Tzu</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Zain.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Zain</b></span> is an incoming senior at Normal Community High School. He enjoys playing tennis and hanging out with friends. He aspires to be an engineer in college and hopes to make a difference in school and beyond.</h6>
+    <h6 ><i >Quote that best describes Zain: "Die with memories, not dreams."</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/base/Contact-1.png" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Allen</b></span> is an incoming freshman that will be attending University High School. He enjoys reading, biking, and playing soccer. As an avid learner, he is currently learning how to code. As of right now, he dreams of becoming a computational economist. One day, he hopes he will be able to create a non-profit.</h6>
+    <h6 ><i >Quote that best describes Allen: "It's not that we have a short space of time, but that we waste much of it. Life is long enough, and it has been given in sufficiently generous measure to allow the accomplishment of the very greatest things, if the whole of it is well invested." - Seneca</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Vivaan.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Vivaan</b></span> is a curious and driven person who is constantly looking for positive change. He is a part of many extracurriculars at NCHS, most notably FBLA, HOSA, Student Council, Class Board, and the Student Equity Action & Advisory Team. At school, he enjoys science, writing, and social studies. During his free time, Vivaan likes to bike, read, help out in the community, and stay up-to-date on current events.</h6>
+    <h6 ><i >Quote that best describes Vivaan: "The power to question is the basis of all human progress" - Indira Gandhi</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Hemanth_Itte.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Hemanth Itte</b></span>  is an incoming senior at Normal Community High School who is planning to study computer science or cybersecurity in college. Currently Hemanth is a technical architect intern with State Farm over the summer. Hemanth participates in many activities such as soccer, wrestling, orchestra, etc. He also enjoys spending time with family and friends and hopes to spread happiness to as many people as possible, for as long as possible, in as many places as possible. Through YGC Hemanth wishes to improve his leadership skills, grow as a person to help the community, and work to achieve his personal goal.</h6>
+    <h6 ><i >Quote that best describes Hemanth: “Learn continually - there's always ‘one more thing’ to learn!”</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Vivaan_Menon.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Vivaan Menon</b></span> is a hardworking individual who likes to complete everything he has as fast as he can. Usually, he prefers to work with others, but that may depend on the project at hand. Additionally, he is very kind and fun to talk to, but at times he may seem irresponsible. </h6>
+    <h6 ><i >Quote that best describes Vivaan: “Don't judge each day by the harvest you reap but by the seeds that you plant.”</i></h6>
+    </td>
+
+    </tr>
+
+    <tr className="row">
+    <td className="col-md-3 center-align"><img src="/images/ygc/2023/Abhinav_Kumar.jpg" alt="Class of 2023" width="150" height="150" className="rounded-circle"></img></td>
+    <td className="col-md-9">
+    <h6 ><span className="font-weight-bold"></span></h6>
+    <h6 ><span><b>Abhinav Kumar</b></span>  is an upcoming senior attending BHS. Driven by a curiosity for business and finance, Abhinav is currently exploring a career in investment banking. He is on the tennis, debate, and golf teams, and also loves watching sports, especially if the Bulls, Seahawks, or Roger Federer are playing! He joined YGC to hone his leadership skills and positively impact the world.</h6>
+    <h6 ><i >Quote that best describes Abhinav: "Loyalty is a two-way street. If I'm asking for it from you, then you're getting it from me." - Harvey Specter</i></h6>
+    </td>
+
+    </tr>
+
+    </tbody>
+
+    </table>
+
+    </div>
+
+    </div>
+
     </div>
 
 
