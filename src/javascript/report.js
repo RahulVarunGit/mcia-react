3212f18eae8f0ca41d913d4ad21b74f26e54d930
Rahul Balivada <rahulvarun.b@gmail.com>
2024-02-24 18:45:00 -0600

    donate link for paypal updated

--- src/javascript/report.js
+++ src/javascript/report.js
@@ -5,23 +5,26 @@ const Report = () => {
 
   return(
 
-    <div className="container">
+   
+<div>
 
 <div>
-<h2 className="center-align"><b>2023 Annual Report</b></h2>
+<h4 className="logo"><img src="/images/base/Logo.jpg"alt="" width="40" height="40" className="rounded" />  for a <span className="cRed">better</span> tomorrow </h4>
+<h4 className="cursiveGrayRight">…Eliminating the gap between abundance and need</h4>
+<p >&nbsp;</p>
 </div>
 
 <div><hr class="hr-line"></hr></div>
 
 <div>
-<iframe src="/pdf/2023AnnualReport.pdf"  width="100%" height="550px"/>
+<h4 className="center-align"><b >Adminstrator is working on this page. Please try again later.</b></h4>
+<p className="center-align"><img src="/images/base/work-in-progress.png" alt="" width="40%" className="rounded" /></p>
 <div><hr class="hr-line"></hr></div>
 
-<h2 className="fbt-tag-line">“Our mission is to promote social justice by meeting needs in the local community and the world through accountable, sustainable philanthropy and by nurturing future generations of servant leaders.”</h2>
+<h4 className="cursiveGray">“Our mission is to promote social justice by meeting needs in the local community and the world through accountable, sustainable philanthropy and by nurturing future generations of servant leaders.”</h4>
 </div>
 
 </div>
-
 
 
 
