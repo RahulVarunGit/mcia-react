4f4f874a7d617d5de93d19cfeb9c79233f75479a
Rahul Balivada <rahulvarun.b@gmail.com>
2024-02-25 11:37:49 -0600

    YGC 2024 wanted page

--- src/YGC-Nav.js
+++ src/YGC-Nav.js
@@ -4,6 +4,7 @@ const YGCnav = () => {
     <div class="row">
       <p>
       <span><b>Flashback:</b></span><span> </span>
+      <span><a className="btn btn-info btn-sm" href="/ygc2024" role="button">2024</a></span><span> </span>
       <span><a className="btn btn-info btn-sm" href="/ygc2023" role="button">2023</a></span><span> </span>
       <span><a className="btn btn-info btn-sm" href="/ygc2022" role="button">2022</a></span><span> </span>
       <span><a className="btn btn-info btn-sm" href="/ygc2021" role="button">2021</a></span><span> </span>
--- src/javascript/YGC/ygc2024.js
+++ src/javascript/YGC/ygc2024.js
@@ -10,14 +10,75 @@ const ygc2024 = () => {
 
 
 
+<div className="container">
+<YGCnav ></YGCnav>
+<div>
+<p className="center-align c-gray"><img src="/images/ygc/2024/YGC_2024.png"alt="YGC 2024" width="300" height="150" className="rounded center-align" ></img></p>
+</div>
 
-        <div className="container">
-        <YGCnav ></YGCnav>
-        <div>
-        <Wip ></Wip>
-        </div>
+<p >&nbsp;&nbsp;</p>
+<div>
+<h2 className="center-align c-gray"><b >WANTED</b></h2>
+</div>
 
-        </div>
+<p >&nbsp;&nbsp;</p>
+<div>
+<h2 className="center-align"><b >Passionate people who want to make a difference!</b></h2>
+</div>
+
+<p >&nbsp;&nbsp;</p>
+<div>
+<h3 className="center-align c-gray"><b >We are bringing together high school students that believe in working together to positively impact our community and world. </b></h3>
+</div>
+
+<div>
+<h3 className="center-align c-gray"><b ><a href="https://tinyurl.com/ygc-overview"><b><span className="c-gray">Program Overview</span></b></a></b></h3>
+</div>
+
+<div>
+<h3 className="center-align c-gray"><b >Students will gain valuable personal leadership and project management knowledge and skills while building a network of peers and leading a project for a non-profit group in our community.</b></h3>
+</div>
+
+<p >&nbsp;&nbsp;</p>
+<p >&nbsp;&nbsp;</p>
+<div className="row">
+
+<div className="col-sm-1 ">
+
+</div>
+
+<div className="col-sm-4 bg-info border">
+
+<p ></p>
+<h3 className="center-align c-white"   ><a href="http://tinyurl.com/ygc-app"><b><span className="c-white">Interested</span></b></a></h3>
+<h4 className="center-align c-ivory"   ><a href="http://tinyurl.com/ygc-app"><b><span className="c-ivory">Let us know with this quick form</span></b></a></h4>
+<p ></p>
+</div>
+
+<div className="col-sm-2 ">
+
+</div>
+
+<div className="col-sm-4  bg-info border">
+
+<p ></p>
+<h3 className="center-align c-white"   ><a href="http://tinyurl.com/ygc-nom"><b><span className="c-white">Nominations</span></b></a></h3>
+<h4 className="center-align c-ivory"   ><a href="http://tinyurl.com/ygc-nom"><b><span className="c-ivory">Share your recommendations here</span></b></a></h4>
+<p ></p>
+</div>
+
+<div className="col-sm-1 ">
+
+</div>
+
+</div>
+
+<p >&nbsp;&nbsp;</p>
+<p >&nbsp;&nbsp;</p>
+<p >&nbsp;</p>
+<p >&nbsp;&nbsp;</p>
+</div>
+
 
 
   );
