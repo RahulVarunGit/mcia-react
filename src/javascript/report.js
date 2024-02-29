f390b83e6c20c4446afe3ea9093f9e298a2e1adc
Rahul Balivada <rahulvarun.b@gmail.com>
2024-02-29 11:12:25 -0600

    report update for 2023

--- src/javascript/report.js
+++ src/javascript/report.js
@@ -8,17 +8,30 @@ const Report = () => {
   const pdfURL = "/pdf/2023AnnualReport.pdf";
 
   return(
-
-   
 <div className="container">
 
-<h2 className="center-align"><b >2024 Annual Report</b></h2>
-<Document file={pdfURL}>
-<Page pageNumber={1} />
- </Document>
+<div>
+<h2 className="center-align"><b>2023 Annual Report</b></h2>
+</div>
+
+<div><hr class="hr-line"></hr></div>
+
+<div>
+<h2 className="center-align"><b ></b></h2>
+<p className="center-align"><img src="/images/report/2023page1.png" alt="" width="100%" className="rounded" /></p>
+<p className="center-align"><img src="/images/report/2023page2.png" alt="" width="100%" className="rounded" /></p>
+<p className="center-align"><img src="/images/report/2023page3.png" alt="" width="100%" className="rounded" /></p>
+<p className="center-align"><img src="/images/report/2023page4.png" alt="" width="100%" className="rounded" /></p>
+<p className="center-align"><img src="/images/report/2023page5.png" alt="" width="100%" className="rounded" /></p>
+<p className="center-align"><img src="/images/report/2023page6.png" alt="" width="100%" className="rounded" /></p>
+<div><hr class="hr-line"></hr></div>
+
+<h2 className="fbt-tag-line">“Our mission is to promote social justice by meeting needs in the local community and the world through accountable, sustainable philanthropy and by nurturing future generations of servant leaders.”</h2>
+</div>
 
 </div>
 
+   
 
 
   );
