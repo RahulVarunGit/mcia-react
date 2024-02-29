27ac6e5c136c249d41092e6a05987b49c99d4b0f
Rahul Balivada <rahulvarun.b@gmail.com>
2024-02-29 10:14:37 -0600

    react-pdf for report

--- src/javascript/report.js
+++ src/javascript/report.js
@@ -1,7 +1,11 @@
 import {useState} from 'react';
 import '../styles/Home.css';
+import { Document, Page, pdfjs  } from 'react-pdf';
+
 
 const Report = () => {
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const pdfURL = "C:/Development/VS-code-workspace/fbt-react-project/public/pdf/2023AnnualReport.pdf";
 
   return(
 
@@ -9,7 +13,10 @@ const Report = () => {
 <div className="container">
 
 <h2 className="center-align"><b >2024 Annual Report</b></h2>
-<iframe src="/pdf/2023AnnualReport.pdf" width="100%" height="500px" />
+<Document file="/pdf/2023AnnualReport.pdf">
+<Page pageNumber={1} />
+ </Document>
+
 </div>
 
 
