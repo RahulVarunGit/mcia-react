18daa5f31c5891f59126dbce576b44aff00efa01
Rahul Balivada <rahulvarun.b@gmail.com>
2024-03-11 18:02:23 -0500

    updated

--- src/javascript/KiteFest.js
+++ src/javascript/KiteFest.js
@@ -5,54 +5,52 @@ const KiteFest = () => {
 
   return(
 
-    <div className="container">
+    
 
+<div className="container">
 
-        <div>
-        <h2 class="center-align"><b>Kite Fest 2023</b></h2>
-        </div>
+<div>
+<h2 class="center-align"><b>Kite Fest 2024</b></h2>
+</div>
 
-        <div><hr class="hr-line"></hr></div>
+<div><hr class="hr-line"></hr></div>
 
-        <div>
-        <h4 class="center-align c-gray"><span class="c-blue">When:</span> Saturday July 15th, 2023, 11:00 am - 5:00 pm CST</h4>
-        </div>
+<div>
+<h4 class="center-align c-gray"><span class="c-blue">When:</span> Saturday July 20th, 2024, 11:00 am - 5:00 pm CST</h4>
+</div>
 
-        <div>
-        <h4 class="center-align c-gray"><span class="c-blue">Cost:</span> <b>FREE</b> for All.</h4>
-        </div>
+<div>
+<h4 class="center-align c-gray"><span class="c-blue">Cost:</span> <b>FREE</b> for All.</h4>
+</div>
 
-        <div>
-        <h4 class="center-align c-gray"><span class="c-blue">Location:</span> Grove Elementary School, Airport Rd, Normal IL 61761</h4>
-        </div>
+<div>
+<h4 class="center-align c-gray"><span class="c-blue">Location:</span> Grove Elementary School, Airport Rd, Normal IL 61761</h4>
+</div>
 
-        <p >&nbsp;&nbsp;</p>
-        <div>
-        <h4 class="center-align c-gray"><b >For a Better Tomorrow presents Kite Fest, an annual fundraising event benefiting local and global communities. This will be our 10th year in a row since starting in 2014. Kite Fest 2023 will be held on Saturday, July 15, 2023 at Grove Elementary School.</b></h4>
-        </div>
+<p >&nbsp;&nbsp;</p>
+<div>
+<h4 class="center-align c-gray"><b >For a Better Tomorrow presents Kite Fest, an annual fundraising event benefiting local and global communities. This will be our 11th year in a row since starting in 2014. Kite Fest 2024 will be held on Saturday, July 20, 2024 at Grove Elementary School.</b></h4>
+</div>
 
-        <div>
-        <h4 class="center-align c-gray"></h4>
-        </div>
+<div>
+<h4 class="center-align c-gray"></h4>
+</div>
 
-        <p ></p>
-        <div>
-        <h4 class="center-align c-gray"><u ></u></h4>
-        </div>
+<p ></p>
+<div>
+<h4 class="center-align c-gray"><u ></u></h4>
+</div>
 
-        <p >&nbsp;&nbsp;</p>
-        <div>
-        <h2 class="center-align"><b ></b></h2>
-        <p class="center-align"><img src="/images/kiteFest/kiteFest2023.png" alt="" width="100%" className="rounded" /></p>
-        <div><hr class="hr-line"></hr></div>
+<p >&nbsp;&nbsp;</p>
+<div>
+<h2 class="center-align"><b ></b></h2>
+<p class="center-align"><img src="/images/kiteFest/kiteFest2024save.png" alt="" width="100%" className="rounded" /></p>
+<div><hr class="hr-line"></hr></div>
 
-        <h2 class="fbt-tag-line">“Our mission is to promote social justice by meeting needs in the local community and the world through accountable, sustainable philanthropy and by nurturing future generations of servant leaders.”</h2>
-        </div>
+<h2 class="fbt-tag-line"> Our mission is to promote social justice by meeting needs in the local community and the world through accountable, sustainable philanthropy and by nurturing future generations of servant leaders. </h2>
+</div>
 
-
-    </div>
-
-
+</div>
 
 
   );
