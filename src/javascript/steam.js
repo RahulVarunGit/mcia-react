2ad5bd967e87e14f0d802b6ef0b43638070d3f48
Rahul Balivada <rahulvarun.b@gmail.com>
2024-07-19 22:30:59 -0500

    fix flyer

--- src/javascript/steam.js
+++ src/javascript/steam.js
@@ -12,7 +12,7 @@ const steam = () => {
       <STEAMnav ></STEAMnav>
       <div>
         <h2 class="center-align"><b ></b></h2>
-        <p class="center-align"><img src="/images/flyers/STEAM2025.jpeg" alt="" width="100%" className="rounded" /></p>
+        <p class="center-align"><img src="/images/flyers/STEAM2025.png" alt="" width="100%" className="rounded" /></p>
         <div><hr class="hr-line"></hr></div>
 
         <h2 class="fbt-tag-line"> Our mission is to promote social justice by meeting needs in the local community and the world through accountable, sustainable philanthropy and by nurturing future generations of servant leaders. </h2>
