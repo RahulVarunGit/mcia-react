19b340f6070f0f95e646349276d28f589eb1947c
Rahul Balivada <rahulvarun.b@gmail.com>
2024-10-14 08:22:12 -0500

    updated package

--- src/javascript/report.js
+++ src/javascript/report.js
@@ -1,37 +1,37 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import '../styles/Home.css';
-import { Document, Page, pdfjs  } from 'react-pdf';
+//import { Document, Page, pdfjs  } from 'react-pdf';
 
 
 const Report = () => {
-  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  //pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
   const pdfURL = "/pdf/2023AnnualReport.pdf";
 
-  return(
-<div className="container">
+  return (
+    <div className="container">
 
-<div>
-<h2 className="center-align"><b>2023 Annual Report</b></h2>
-</div>
+      <div>
+        <h2 className="center-align"><b>2023 Annual Report</b></h2>
+      </div>
 
-<div><hr class="hr-line"></hr></div>
+      <div><hr class="hr-line"></hr></div>
 
-<div>
-<h2 className="center-align"><b ></b></h2>
-<p className="center-align"><img src="/images/report/2023page1.png" alt="" width="100%" className="rounded" /></p>
-<p className="center-align"><img src="/images/report/2023page2.png" alt="" width="100%" className="rounded" /></p>
-<p className="center-align"><img src="/images/report/2023page3.png" alt="" width="100%" className="rounded" /></p>
-<p className="center-align"><img src="/images/report/2023page4.png" alt="" width="100%" className="rounded" /></p>
-<p className="center-align"><img src="/images/report/2023page5.png" alt="" width="100%" className="rounded" /></p>
-<p className="center-align"><img src="/images/report/2023page6.png" alt="" width="100%" className="rounded" /></p>
-<div><hr class="hr-line"></hr></div>
+      <div>
+        <h2 className="center-align"><b ></b></h2>
+        <p className="center-align"><img src="/images/report/2023page1.png" alt="" width="100%" className="rounded" /></p>
+        <p className="center-align"><img src="/images/report/2023page2.png" alt="" width="100%" className="rounded" /></p>
+        <p className="center-align"><img src="/images/report/2023page3.png" alt="" width="100%" className="rounded" /></p>
+        <p className="center-align"><img src="/images/report/2023page4.png" alt="" width="100%" className="rounded" /></p>
+        <p className="center-align"><img src="/images/report/2023page5.png" alt="" width="100%" className="rounded" /></p>
+        <p className="center-align"><img src="/images/report/2023page6.png" alt="" width="100%" className="rounded" /></p>
+        <div><hr class="hr-line"></hr></div>
 
-<h2 className="fbt-tag-line">“Our mission is to promote social justice by meeting needs in the local community and the world through accountable, sustainable philanthropy and by nurturing future generations of servant leaders.”</h2>
-</div>
+        <h2 className="fbt-tag-line">“Our mission is to promote social justice by meeting needs in the local community and the world through accountable, sustainable philanthropy and by nurturing future generations of servant leaders.”</h2>
+      </div>
 
-</div>
+    </div>
 
-   
+
 
 
   );
