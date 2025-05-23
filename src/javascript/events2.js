b1ae1ddebc7bbf604dcf3b5936062aae7128e112
Rahul Balivada <rahulvarun.b@gmail.com>
2025-05-23 14:05:30 -0500

    updated gal link

--- src/javascript/events2.js
+++ src/javascript/events2.js
@@ -36,7 +36,7 @@ const Events2 = () => {
             completed = completed + "<h5 >" + event.where2 + "</h5>";
             completed = completed + "<h5 >When : " + event.when + "</h5>";
             console.log("---gal link : " + event.galleryLink);
-            if (event.galleryLink === undefined) {
+            if (event.galleryLink === "") {
                 completed = completed + "<h5 ><a type=\"button\" className=\"btn btn-primary btn-lg\" href=\"/gallery/ \">" + event.btnGallery + "</a></h5>";
             } else {
                 completed = completed + "<h5 ><a type=\"button\" className=\"btn btn-primary btn-lg\" href=\"/galleryView/" + event.galleryLink + "\">" + event.btnGallery + "</a></h5>";
