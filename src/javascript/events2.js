b3ecf8e2f7b715fb762c7ac3804cb9e8ad35eaf3
Rahul Balivada <rahulvarun.b@gmail.com>
2025-05-23 13:30:24 -0500

    gallery link to event updated for no images

--- src/javascript/events2.js
+++ src/javascript/events2.js
@@ -35,7 +35,12 @@ const Events2 = () => {
             completed = completed + "<h5 >Where : " + event.where + "</h5>";
             completed = completed + "<h5 >" + event.where2 + "</h5>";
             completed = completed + "<h5 >When : " + event.when + "</h5>";
-            completed = completed + "<h5 ><a type=\"button\" className=\"btn btn-primary btn-lg\" href=\"/galleryView/" + event.galleryLink + "\">" + event.btnGallery + "</a></h5>";
+            if (event.galleryLink = "") {
+                completed = completed + "<h5 ><a type=\"button\" className=\"btn btn-primary btn-lg\" href=\"/galleryView/" + event.gallery + "\">" + "</a></h5>";
+            } else {
+                completed = completed + "<h5 ><a type=\"button\" className=\"btn btn-primary btn-lg\" href=\"/galleryView/" + event.galleryLink + "\">" + event.btnGallery + "</a></h5>";
+            }
+
             completed = completed + "</div></div>";
             completed = completed + "<div><hr className=\"hr-line\"></hr></div>";
         }
