e7bd590998de5d157ef235fffe73b07b5e71235e
Rahul Balivada <rahulvarun.b@gmail.com>
2025-05-23 13:13:13 -0500

    updated events source

--- src/App.js
+++ src/App.js
@@ -18,19 +18,8 @@ import Flyer from './javascript/flyer';
 import Events2 from './javascript/events2';
 import Event from './javascript/event';
 
-import Volleyball from './javascript/volleyball';
-import Badminton from './javascript/badminton';
-import Pickleball from './javascript/pickleball';
-import RepublicDay from './javascript/republicDay';
-import WomensDay from './javascript/womensDay';
-import Chess from './javascript/chess';
 import ChessPairings from './javascript/chessPairings';
 import ChessStandings from './javascript/chessStandings';
-import TalentShow from './javascript/talentShow';
-import Run from './javascript/run';
-import YouthPickelball from './javascript/youth-pickleball';
-import IndependenceDay from './javascript/independence-day';
-import Health from './javascript/health';
 
 import Login from './javascript/login';
 import Register from './javascript/register';
@@ -39,6 +28,33 @@ import Gallery from './javascript/gallery';
 import GalleryView from './javascript/galleryView';
 import GalleryItem from './javascript/galleryItem';
 
+/*
+import Volleyball from './javascript/volleyball';
+import Badminton from './javascript/badminton';
+import Pickleball from './javascript/pickleball';
+import RepublicDay from './javascript/republicDay';
+import WomensDay from './javascript/womensDay';
+import Chess from './javascript/chess';
+import TalentShow from './javascript/talentShow';
+import Run from './javascript/run';
+import YouthPickelball from './javascript/youth-pickleball';
+import IndependenceDay from './javascript/independence-day';
+import Health from './javascript/health';
+
+
+            <Route path="/volleyball" element={<Volleyball />} /> 
+            <Route path="/badminton" element={<Badminton />} />
+            <Route path="/pickleball" element={<Pickleball />} />
+            <Route path="/republic-day" element={<RepublicDay />} />
+            <Route path="/womensDay" element={<WomensDay />} />
+            <Route path="/chess" element={<Chess />} />
+            <Route path="/talentShow" element={<TalentShow />} />
+            <Route path="/run" element={<Run />} />
+            <Route path="/youth-pickleball" element={<YouthPickelball />} />
+            <Route path="/independence-day" element={<IndependenceDay />} />
+            <Route path="/health" element={<Health />} />
+            
+*/
 
 
 
@@ -70,19 +86,8 @@ function App() {
             <Route path="/flyer/:name" element={<Flyer />} />
             <Route path="/events" element={<Events2 />} />
             <Route path="/event/:name" element={<Event />} />
-            <Route path="/volleyball" element={<Volleyball />} />
-            <Route path="/badminton" element={<Badminton />} />
-            <Route path="/pickleball" element={<Pickleball />} />
-            <Route path="/republic-day" element={<RepublicDay />} />
-            <Route path="/womensDay" element={<WomensDay />} />
-            <Route path="/chess" element={<Chess />} />
             <Route path="/chess/pairings/:group" element={<ChessPairings />} />
             <Route path="/chess/standings/:group" element={<ChessStandings />} />
-            <Route path="/talentShow" element={<TalentShow />} />
-            <Route path="/run" element={<Run />} />
-            <Route path="/youth-pickleball" element={<YouthPickelball />} />
-            <Route path="/independence-day" element={<IndependenceDay />} />
-            <Route path="/health" element={<Health />} />
 
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
