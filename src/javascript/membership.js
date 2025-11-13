f797d8a45f98c4c14121010e04254fa04098def2
Rahul Balivada <rahulvarun.b@gmail.com>
2025-11-13 12:36:27 -0600

    2026 Registration added

--- src/javascript/membership.js
+++ src/javascript/membership.js
@@ -4,7 +4,6 @@ import '../styles/Home.css';
 const Membership = () => {
 
   return (
-
     <div className="container">
 
       <p >&nbsp;</p>
@@ -16,21 +15,23 @@ const Membership = () => {
       <div><hr class="hr-line"></hr></div>
 
       <div>
-        <h4 className="center-align">2025 MCIA Registration is open.</h4>
+        <h4 className="center-align">2026 MCIA Registration is open.</h4>
         <h4 className="center-align">Member enjoy <b>Exclusive Discounts</b> on ticketed events.</h4>
         <h4 className="center-align">Members-Only Access to stage performances & participation opportunities.</h4>
       </div>
 
       <div className="row">
 
-        <h5 className="center-align"><a type="button" class="btn btn-primary btn-lg" href="https://www.zeffy.com/en-US/ticketing/mciamclean-county-india-association-memberships--2025">Register here</a></h5>
+        <h5 className="center-align"><a type="button" class="btn btn-primary btn-lg" href="https://www.zeffy.com/en-US/ticketing/mcia-2026-annual-membership">Register here</a></h5>
       </div>
 
       <p >&nbsp;</p>
+      <div className="row">
+
+        <h4 className="center-align"><a href="https://www.zeffy.com/en-US/ticketing/mciamclean-county-india-association-memberships--2025">Click here if you still need 2025 membership.</a></h4>
+      </div>
+
     </div>
-
-
-
 
 
   );
